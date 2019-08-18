f3f15c4d768b9c6a5163b76aeb1d65b9b1e178cf
Rafael López Martínez <ryalopez@gmail.com>
2019-08-18 18:14:15 +0200

    LeerDirectorio

--- Aplicación/Biblioteca/LeerDirectorio.m
+++ Aplicación/Biblioteca/LeerDirectorio.m
@@ -11,25 +11,29 @@ function [Hs, DirOlas, VelViento, DirViento] = LeerDirectorio(observatorio)
     if length(ficheros) > 0
         disp(observatorio);
         load(strcat( observatorio, '\', ficheros(1).name));
-        datos=data_rem_no0.yeardata;
-        cabeceras=data_rem_no0.yearvar;
-        tabla=array2table(datos,'VariableNames',cabeceras);
-        z=size(tabla);
-        ncolumnas=z(2);
-        if ncolumnas == 18
-            tiempos=array2table(datetime(table2array(tabla(:,1)), ...
-                table2array(tabla(:,2)), table2array(tabla(:,3)), ...
-                table2array(tabla(:,4)), ...
-                table2array(tabla(:,5)),0),'VariableNames',{'Tiempo'});
-            tabla(:,1:5)=[];   % borrar columna año, mes, día, hora, minuto
-        else
-            tiempos=array2table(datetime(table2array(tabla(:,1)), ...
-                table2array(tabla(:,2)), table2array(tabla(:,3)), ...
-                table2array(tabla(:,4)), ...
-                0,0),'VariableNames',{'Tiempo'});
-            tabla(:,1:4)=[];   % borrar columna año, mes, día, hora, minuto        
-        end
-        t=table2array(tabla(:,1:7));
+        annos = length(data_rem_no0);
+        t=[];
+        for i=1:annos
+            datos=data_rem_no0(i).yeardata;
+            cabeceras=data_rem_no0(i).yearvar;
+            tabla=array2table(datos,'VariableNames',cabeceras);
+            z=size(tabla);
+            ncolumnas=z(2);
+            if ncolumnas == 18
+                tiempos=array2table(datetime(table2array(tabla(:,1)), ...
+                    table2array(tabla(:,2)), table2array(tabla(:,3)), ...
+                    table2array(tabla(:,4)), ...
+                    table2array(tabla(:,5)),0),'VariableNames',{'Tiempo'});
+                tabla(:,1:5)=[];   % borrar columna año, mes, día, hora, minuto
+            else
+                tiempos=array2table(datetime(table2array(tabla(:,1)), ...
+                    table2array(tabla(:,2)), table2array(tabla(:,3)), ...
+                    table2array(tabla(:,4)), ...
+                    0,0),'VariableNames',{'Tiempo'});
+                tabla(:,1:4)=[];   % borrar columna año, mes, día, hora, minuto
+            end
+            t=[t; table2array(tabla(:,1:7))];
+        end        
         z = size(t);
         kolmo=[];
         for i = 1:z(2)
